891d23fdad86cbc0984d923e98f262dc5fc26f8d
Conor McDonagh Rollo <c00260445@itcarlow.ie>
2024-01-14 06:38:01 +0000

    preparing for release

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -12,11 +13,21 @@ public class Player : MonoBehaviour
     public float groundDistance = 0.2f;
     public LayerMask groundMask;
 
+    public Animator rot_anim;
+    public Animator anim;
+
+    public AudioClip player_jump1;
+    public AudioClip player_jump2;
+    public AudioClip player_land;
+
+    public AudioSource player_walk_as;
+
     // Private
     private Rigidbody rb;
-    public bool isGrounded;
+    public bool isGrounded = true;
     private Vector3 movement;
     public int jumpsRemaining = 2;
+    float walkSoundTimer = .35f;
 
     // Static
     public static AudioSource audioSource;
@@ -29,10 +40,34 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        bool previouslyGrounded = isGrounded;
+        float previousVelocity = rb.velocity.y;
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        anim.SetBool("Jumping", !isGrounded);
+        if (!previouslyGrounded && isGrounded && previousVelocity < -1f)
+        {
+            audioSource.PlayOneShot(player_land);
+        }
 
         float h = Input.GetAxis("Horizontal");
         movement = new Vector3(h, 0f, 0f) * movementSpeed;
+        if (movement.x > 0f || movement.x < 0f)
+        {
+            anim.SetBool("Walking", true);
+            rot_anim.SetInteger("dir", (int)movement.x);
+            walkSoundTimer -= Time.deltaTime;
+            if (walkSoundTimer < 0f && isGrounded)
+            {
+                walkSoundTimer = .35f;
+                player_walk_as.pitch = Random.Range(.75f, 1.25f);
+                player_walk_as.Play();
+            }
+        }
+        else
+        {
+            anim.SetBool("Walking", false);
+            rot_anim.SetInteger("dir", 0);
+        }
 
         if (Input.GetButtonDown("Jump"))
         {
@@ -62,5 +97,13 @@ public class Player : MonoBehaviour
     {
         rb.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode.Impulse);
         isGrounded = false;
+        if (jumpsRemaining == 1)
+        {
+            audioSource.PlayOneShot(player_jump1);
+        }
+        else
+        {
+            audioSource.PlayOneShot(player_jump2);
+        }
     }
 }
